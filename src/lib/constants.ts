2e3fca31a543608bda75bebbfd33545ceba9d602
aneezv <aneesvemmulli@gmail.com>
2026-05-18 22:16:32 +0530

    feat: update application version to v0.2.0

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -5,7 +5,7 @@
  */
 
 // Centralized version number for the application
-export const APP_VERSION = 'v0.1.3';
+export const APP_VERSION = 'v0.2.0';
 
 // Add any other global enterprise constants here in the future:
 // export const SUPPORT_EMAIL = 'support@peregrinehub.com';
